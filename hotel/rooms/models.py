ebe1275dd1aa1ba4ba64030f8c99acc8765f7ee4
KenKimwe <kimwe.king@gmail.com>
2026-02-19 13:29:28 +0300

    feat(contact): link contact form frontend to Django backend and fix submission flow

--- hotel/rooms/models.py
+++ hotel/rooms/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    fullname=models.CharField(max_length=50)
+    email=models.EmailField()
+    subject=models.CharField(max_length=100)
+    message=models.TextField()
+    created_at=models.DateTimeField( null=True, blank=True, auto_now=True)
+
+    def __str__(self):
+        return self.fullname
--- hotel/rooms/views.py
+++ hotel/rooms/views.py
@@ -1,19 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
+
 
 def home(request):
-    return render(request, 'rooms/home.html')
+    return render(request, "rooms/home.html")
+
 
 def about(request):
-    return render(request, 'rooms/about.html')
+    return render(request, "rooms/about.html")
+
 
 def rooms_page(request):
-    return render(request, 'rooms/rooms.html')
+    return render(request, "rooms/rooms.html")
+
 
 def amenities(request):
-    return render(request, 'rooms/amenities.html')
+    return render(request, "rooms/amenities.html")
+
 
 def book(request):
-    return render(request, 'rooms/book.html')
+    return render(request, "rooms/book.html")
 
 def contact(request):
-    return render(request, 'rooms/contact.html')
+    if request.method == "POST":
+        fullname = request.POST.get("fullname")
+        email = request.POST.get("email")
+        subject = request.POST.get("subject")
+        message = request.POST.get("message")
+
+        Contact.objects.create(
+            fullname=fullname,
+            email=email,
+            subject=subject,
+            message=message,
+        )
+
+        return redirect("home")
+    return render(request, "rooms/contact.html")
